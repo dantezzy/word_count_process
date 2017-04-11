addfa083faf06403da18abe861f45cf41e3ee0a7
dantezzy <markzhao0607@gmail.com>
2017-04-11 14:15:07 -0400

    create word pair cluster program

---
+++ word_pair_cluster.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-11 14:21:03
+# @Author  : ziyi Zhao
+# @Version : 1.0
+# 1.0 : use word2vec to train the model
+#       and use the kmean to do the cluster
+
+import os
