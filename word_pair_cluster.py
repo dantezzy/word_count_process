8c37d81bc11d17229bdb537232671c3ee7f8d1b9
dantezzy <markzhao0607@gmail.com>
2017-04-12 16:53:17 -0400

    add sklearn kmean package

--- word_pair_cluster.py
+++ word_pair_cluster.py
@@ -8,7 +8,6 @@
 
 import os
 import nose
-import numpy
 import scipy
 import gensim
 import logging
@@ -22,6 +21,15 @@ from multiprocessing import Process, Lock, Manager
 # word2vec process
 import gensim
 # kmean cluster
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
 
 ###############################################################################################################################################
 # load the document from pickle file
