f2e7b177a2c74624ada9a4ce52e389990a1b34b5
dantezzy <markzhao0607@gmail.com>
2017-05-18 13:43:40 -0400

    add wordnet group function

--- generate_word_count_OMDB.py
+++ generate_word_count_OMDB.py
@@ -45,23 +45,43 @@ def load_pickle_dict(path):
 # single process convert real word into  group word
 def single_process_group_word_converter(dataset,group_word_collection,start,end,group_dict):
 
-	for word_pair_index in xrange(start,end):
-# iterate each word pair in training dictionary
-		group1 = ''
-		group2 = ''
-# iterate each word group in represented word relationships
-		for index,group_word in group_dict.items():
-# if a word in specific word group
-			if dataset[word_pair_index][0] in group_word:
-				group1 = index
-			if dataset[word_pair_index][1] in group_word:
-				group2 = index
-		pair = list()
-		pair.append(group1)
-		pair.append(group2)
-		print "Process word pair:",word_pair_index
+	mode = "wordnet"
 
-		group_word_collection.append(pair)
+	if mode == "kmean":
+		for word_pair_index in xrange(start,end):
+# iterate each word pair in training dictionary
+			group1 = ''
+			group2 = ''
+# iterate each word group in represented word relationships
+			for index,group_word in group_dict.items():
+# if a word in specific word group
+				if dataset[word_pair_index][0] in group_word:
+					group1 = index
+				if dataset[word_pair_index][1] in group_word:
+					group2 = index
+			pair = list()
+			pair.append(group1)
+			pair.append(group2)
+			print "Process word pair:",word_pair_index
+
+			group_word_collection.append(pair)
+
+	if mode == "wordnet":
+		for word_pair_index in xrange(start,end):
+# iterate each word pair in training dictionary
+			group1 = dataset[word_pair_index][0]
+			group2 = dataset[word_pair_index][1]
+# if a word in specific word group
+			if group_dict.has_key(dataset[word_pair_index][0]):
+				group1 = group_dict[dataset[word_pair_index][0]]
+			if group_dict.has_key(dataset[word_pair_index][1]):
+				group2 = group_dict[dataset[word_pair_index][1]]
+			pair = list()
+			pair.append(group1)
+			pair.append(group2)
+			print "Process word pair:",word_pair_index
+
+			group_word_collection.append(pair)
 
 ###############################################################################################################################################
 # multi process convert real word into  group word
@@ -90,22 +110,71 @@ def multi_process_group_word_converter(dataset,number_of_process,group_dict):
 
 ###############################################################################################################################################
 # single process parser
-def single_process_word_count(dataset,word_pair_collection,start,end,word_dict):
+def single_process_word_count(dataset,word_pair_collection,start,end,word_dict,group_dict):
 
-	for doc_count in xrange(start,end):
-		document_word_count_temp = list()
-		for word_pair in word_dict:
-			frequency = dataset[doc_count].count(word_pair)
-			# if frequency  != 0:
-				# print frequency
-			document_word_count_temp.append(frequency)
-		print "Doc:",doc_count
-		#print document_word_count_temp
-		word_pair_collection.update({doc_count:document_word_count_temp})
+	mode = "wordnet"
+
+	if mode == "kmean":
+		for doc_count in xrange(start,end):
+
+			temp_dataset_list = list()
+			for word_pair_in_dataset in dataset[doc_count]:
+				group1 = ''
+				group2 = ''
+				for index,group_word in group_dict.items():
+	# if a word in specific word group
+					if word_pair_in_dataset[0] in group_word:
+						group1 = index
+					if word_pair_in_dataset[1] in group_word:
+						group2 = index
+				pair = list()
+				pair.append(group1)
+				pair.append(group2)
+				temp_dataset_list.append(pair)
+
+			document_word_count_temp = list()
+			# print temp_dataset_list
+			for word_pair in word_dict:
+				frequency = temp_dataset_list.count(word_pair)
+				# if frequency  != 0:
+					# print frequency
+				document_word_count_temp.append(frequency)
+			print "Doc:",doc_count
+			#print document_word_count_temp
+			word_pair_collection.update({doc_count:document_word_count_temp})
+
+	if mode == "wordnet":
+		for doc_count in xrange(start,end):
+
+			temp_dataset_list = list()
+			for word_pair_in_dataset in dataset[doc_count]:
+				group1 = ''
+				group2 = ''
+				for index,group_word in group_dict.items():
+	# if a word in specific word group
+					if word_pair_in_dataset[0] in group_word:
+						group1 = index
+					if word_pair_in_dataset[1] in group_word:
+						group2 = index
+				pair = list()
+				pair.append(group1)
+				pair.append(group2)
+				temp_dataset_list.append(pair)
+
+			document_word_count_temp = list()
+			# print temp_dataset_list
+			for word_pair in word_dict:
+				frequency = temp_dataset_list.count(word_pair)
+				# if frequency  != 0:
+					# print frequency
+				document_word_count_temp.append(frequency)
+			print "Doc:",doc_count
+			#print document_word_count_temp
+			word_pair_collection.update({doc_count:document_word_count_temp})
 
 ###############################################################################################################################################
 # multi process parser
-def multi_process_word_count(dataset,number_of_process,word_dict):
+def multi_process_word_count(dataset,number_of_process,word_dict,group_dict):
 
 	manager = Manager()
 	word_pair_collection = manager.dict()
@@ -113,12 +182,12 @@ def multi_process_word_count(dataset,number_of_process,word_dict):
 	segment = len(dataset)/number_of_process
 	word_count_list = list()
 
-	all_processes = [Process(target=single_process_word_count, args=(dataset, word_pair_collection,x*segment, (x+1)*segment,word_dict)) for x in xrange(0,number_of_process)]
+	all_processes = [Process(target=single_process_word_count, args=(dataset, word_pair_collection,x*segment, (x+1)*segment,word_dict,group_dict)) for x in xrange(0,number_of_process)]
 
 	for p in all_processes:
 		p.start()
 
-	P_last=Process(target=single_process_word_count, args=(dataset, word_pair_collection,number_of_process*segment, len(dataset),word_dict))
+	P_last=Process(target=single_process_word_count, args=(dataset, word_pair_collection,number_of_process*segment, len(dataset),word_dict,group_dict))
 	if number_of_process*segment < len(dataset):
 		P_last.start()
 
@@ -141,7 +210,13 @@ def multi_process_word_count(dataset,number_of_process,word_dict):
 # run separate parts
 def run(train_dataset_pickle_path,train_dictionary_pickle_path,test_dataset_pickle_path):
 
-	group_dict = load_pickle_dict("./group_dictionary.pkl")
+	mode = "wordnet"
+
+	if mode == "kmean":
+		group_dict = load_pickle_dict("./group_dictionary.pkl")
+	if mode == "wordnet":
+		group_dict = load_pickle_dict("./represented_word_corresponding_relationship.pkl")
+
 	# for key,value in group_dict.items():
 	# 	print (key,value)
 
@@ -152,77 +227,79 @@ def run(train_dataset_pickle_path,train_dictionary_pickle_path,test_dataset_pick
 	print "The length of group dict is:",len(group_word_dictionary)
 	group_word_dictionary.sort()
 	group_word_dictionary_without_duplicate = list(group_word_dictionary for group_word_dictionary,_ in itertools.groupby(group_word_dictionary))
-	print group_word_dictionary_without_duplicate
+	# print group_word_dictionary_without_duplicate
 	print "The length of group dict set is:",len(group_word_dictionary_without_duplicate)
   	with open('./group_word_dictionary.pkl','w') as f:
 	    pickle.dump(group_word_dictionary_without_duplicate,f)
 ######################################################################################################################
 
+# process training and testing dataset
+	train_dataset = load_pickle(train_dataset_pickle_path)
 
-	# train_dataset = load_pickle(train_dataset_pickle_path)
+	test_dataset = load_pickle(test_dataset_pickle_path)
 
-	# test_dataset = load_pickle(test_dataset_pickle_path)
+	train_dataset_word_count_path = open('./train_dataset_word_count.txt','w')
+	train_dataset_word_count_pickle_path = open('./train_dataset_word_count.pkl','w')
+	train_count = 0
+	test_count = 0
 
-	# train_dataset_word_count_path = open('./train_dataset_word_count.txt','w')
-	# train_dataset_word_count_pickle_path = open('./train_dataset_word_count.pkl','w')
-	# train_count = 0
-	# test_count = 0
+	segment = len(train_dataset)/5
+	result = list()
 
-	# segment = len(train_dataset)/5
-	# result = list()
+# batch processing
+	for index in xrange(0,5):
+		temp_train_dataset = list()
+		train_dataset_word_count_list = list()
+		for x in xrange(index*segment, (index+1)*segment):
+			temp_train_dataset.append(train_dataset[x])
+		train_dataset_word_count_list = multi_process_word_count(temp_train_dataset,8,group_word_dictionary_without_duplicate,group_dict)
+		result.extend(train_dataset_word_count_list)
+		train_count += len(train_dataset_word_count_list)
 
-	# for index in xrange(0,5):
-	# 	temp_train_dataset = list()
-	# 	train_dataset_word_count_list = list()
-	# 	for x in xrange(index*segment, (index+1)*segment):
-	# 		temp_train_dataset.append(train_dataset[x])
-	# 	train_dataset_word_count_list = multi_process_word_count(temp_train_dataset,8,train_dictionary)
-	# 	result.extend(train_dataset_word_count_list)
-	# 	train_count += len(train_dataset_word_count_list)
+		dictionary_result='./train_dataset_word_count_'
+		dictionary_result+=str(index+1)
+		dictionary_result+='.pkl'
 
-	# 	dictionary_result='./train_dataset_word_count_'
-	# 	dictionary_result+=str(index+1)
-	# 	dictionary_result+='.pkl'
+	  	with open(dictionary_result,'w') as f:
+		    pickle.dump(train_dataset_word_count_list,f)
 
-	#   	with open(dictionary_result,'w') as f:
-	# 	    pickle.dump(train_dataset_word_count_list,f)
+	if 5*segment < len(train_dataset):
+		temp_train_dataset = list()
+		train_dataset_word_count_list = list()
+		for x in xrange(5*segment, len(train_dataset)):
+			temp_train_dataset.append(train_dataset[x])
+		train_dataset_word_count_list = multi_process_word_count(temp_train_dataset,8,train_dictionary,group_dict)
+		result.extend(train_dataset_word_count_list)
+		train_count += len(train_dataset_word_count_list)
 
-	# if 5*segment < len(train_dataset):
-	# 	temp_train_dataset = list()
-	# 	train_dataset_word_count_list = list()
-	# 	for x in xrange(5*segment, len(train_dataset)):
-	# 		temp_train_dataset.append(train_dataset[x])
-	# 	train_dataset_word_count_list = multi_process_word_count(temp_train_dataset,8,train_dictionary)
-	# 	result.extend(train_dataset_word_count_list)
-	# 	train_count += len(train_dataset_word_count_list)
+		dictionary_result='./train_dataset_word_count_last.pkl'
+		dictionary_result+='.pkl'
 
-	# 	dictionary_result='./train_dataset_word_count_last.pkl'
-	# 	dictionary_result+='.pkl'
+	  	with open(dictionary_result,'w') as f:
+		    pickle.dump(train_dataset_word_count_list,f)
 
-	#   	with open(dictionary_result,'w') as f:
-	# 	    pickle.dump(train_dataset_word_count_list,f)
-
-	# train_dataset_word_count_path = open('./train_dataset_word_count.txt','a')
-	# for item in result:
-	# 	train_dataset_word_count_path.write("%s\n" % item)
- #  	with open('./train_dataset_word_count.pkl','w') as f:
-	#     pickle.dump(result,f)
+# save all in one file, txt and pickle
+	train_dataset_word_count_path = open('./train_dataset_word_count.txt','a')
+	for item in result:
+		train_dataset_word_count_path.write("%s\n" % item)
+  	# with open('./train_dataset_word_count.pkl','w') as f:
+	  #   pickle.dump(result,f)
 
 
-	# test_dataset_word_count_list = multi_process_word_count(test_dataset,8,train_dictionary)
-	# test_count = len(test_dataset_word_count_list)
-	# test_dataset_word_count_path = open('./test_dataset_word_count.txt','w')
-	# for item in test_dataset_word_count_list:
-	# 	test_dataset_word_count_path.write("%s\n" % item)
- #  	with open('./test_dataset_word_count.pkl','w') as f:
-	#     pickle.dump(test_dataset_word_count_list,f)
+	test_dataset_word_count_list = multi_process_word_count(test_dataset,8,group_word_dictionary_without_duplicate,group_dict)
+	test_count = len(test_dataset_word_count_list)
+	test_dataset_word_count_path = open('./test_dataset_word_count.txt','w')
+	for item in test_dataset_word_count_list:
+		test_dataset_word_count_path.write("%s\n" % item)
+  	with open('./test_dataset_word_count.pkl','w') as f:
+	    pickle.dump(test_dataset_word_count_list,f)
 
-	# print "Number of train document: ",len(result)
-	# print "Number of test document: ",test_count
+	print "Number of train document: ",len(result)
+	print "Number of test document: ",test_count
 
-	# print "Number of words in dictionary: ", len(train_dictionary)
-	# # print len(train_dataset)
-	# # print len(test_dataset)
+	print "Number of words in dictionary: ", len(group_word_dictionary_without_duplicate)
+	print len(train_dataset)
+	print len(test_dataset)
 
 ###############################################################################################################################################
 if __name__ == '__main__':
